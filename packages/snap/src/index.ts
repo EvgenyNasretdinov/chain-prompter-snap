2b369f0c3208e9e3e2029d8cde39a72aa4af1d8c
Evgeny Nasretdinov <evgeny.nasretdinov@gmail.com>
2022-10-25 19:17:53 +0200

    initial snap for chain-prompter-snap

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,41 +1,34 @@
-import { OnRpcRequestHandler } from '@metamask/snap-types';
+import { OnTransactionHandler } from '@metamask/snap-types';
 
-/**
- * Get a message from the origin. For demonstration purposes only.
- *
- * @param originString - The origin string.
- * @returns A message based on the origin.
- */
-export const getMessage = (originString: string): string =>
-  `Hello, ${originString}!`;
+const CHAIN_PROMPTER_API_ENDPOINT = 'https://hrtx-svfl63fyhq-lm.a.run.app'
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns `null` if the request succeeded.
- * @throws If the request method is not valid for this snap.
- * @throws If the `snap_confirm` call failed.
- */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
-  switch (request.method) {
-    case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: getMessage(origin),
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
-    default:
-      throw new Error('Method not found.');
+export const onTransaction: OnTransactionHandler = async ({ transaction, chainId }) => {
+  const {to, data} = transaction
+
+  const response = await fetch(`${CHAIN_PROMPTER_API_ENDPOINT}/chainId/${chainId.split(':')[1]}/txData`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(
+      {
+        to,
+        data
+      }
+      )
+    })
+
+    if (!response.ok) {
+      throw new Error(
+        `Unable to fetch from Chain Prompter "${chainId}": ${response.status} ${response.statusText}.`,
+        )
+    }
+
+    const output = await response.text()
+
+  return {
+    insights: {
+      output
+    }
   }
 };
